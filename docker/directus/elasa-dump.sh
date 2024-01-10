d4f12ea13869c23d02634d4a67b642e7a1d07df3
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-01-10 15:16:35 +0100

    Does not drop directus/elasa tables before load

--- docker/directus/elasa-dump.sh
+++ docker/directus/elasa-dump.sh
@@ -1,4 +1,4 @@
 source ../../.env
-docker-compose exec postgres pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" --clean --if-exists -n public --schema-only -T directus_* --clean > elasa-schema.sql
+docker-compose exec postgres pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" -n public --schema-only -T directus_* | grep -v "CREATE SCHEMA public" > elasa-schema.sql
 docker-compose exec postgres pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" -n public --data-only --column-inserts -T directus_* -T pois > elasa-data-tmp.sql
 perl pg_dump_sort.perl elasa-data-tmp.sql > elasa-data.sql && rm elasa-data-tmp.sql
