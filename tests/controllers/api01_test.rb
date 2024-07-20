5e3eb4ad9a4735cadbefcdd36a6f168144c479d7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-07-20 15:47:17 +0200

    Fix TU

--- tests/controllers/api01_test.rb
+++ tests/controllers/api01_test.rb
@@ -7,17 +7,16 @@ require 'json-schema'
 require_relative '../test_helper'
 
 class Api01ControllerTest < ActionController::TestCase
-  def setup
-    PG.connect(host: ENV.fetch('POSTGRES_HOST', nil), dbname: ENV['RAILS_ENV'] == 'test' ? 'test' : 'postgres', user: ENV.fetch('POSTGRES_USER', nil), password: ENV.fetch('POSTGRES_PASSWORD', nil)) { |conn|
-      conn.exec(File.new('docker/directus/elasa-schema.sql').read
-        .gsub('DROP SCHEMA IF EXISTS public;', '')
-        .gsub('CREATE SCHEMA public;', ''))
-      conn.exec(File.new('tests/elasa-fixtures.sql').read)
-      conn.exec(File.new('lib/api.sql').read)
-    }
-
-    @schema = YAML.safe_load_file('public/elasa-0.1.swagger.yaml')
-  end
+  @init = PG.connect(host: ENV.fetch('POSTGRES_HOST', nil), dbname: ENV['RAILS_ENV'] == 'test' ? 'test' : 'postgres', user: ENV.fetch('POSTGRES_USER', nil), password: ENV.fetch('POSTGRES_PASSWORD', nil)) { |conn|
+    conn.exec('DROP SCHEMA IF EXISTS public CASCADE')
+    conn.exec('CREATE SCHEMA public')
+    conn.exec('CREATE EXTENSION IF NOT EXISTS postgis')
+    conn.exec(File.new('docker/directus/elasa-schema.sql').read)
+    conn.exec(File.new('docker/directus/directus-schema.sql').read)
+    conn.exec(File.new('tests/elasa-fixtures.sql').read)
+    conn.exec(File.new('lib/api-01.sql').read)
+  }
+  @@schema = YAML.safe_load_file('public/elasa-0.1.swagger.yaml')
 
   def schema_for(path)
     path = "/{project}/{theme}/#{path}".gsub('/', '~1')
@@ -28,14 +27,14 @@ class Api01ControllerTest < ActionController::TestCase
     get :settings, params: { project: 'test', theme: 'theme' }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('settings.json'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('settings.json'))
   end
 
   def test_articles
     get :articles, params: { project: 'test', theme: 'theme' }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('articles.json'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('articles.json'))
 
     assert_equal 1, json.size
   end
@@ -44,45 +43,45 @@ class Api01ControllerTest < ActionController::TestCase
     get :menu, params: { project: 'test', theme: 'theme' }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('menu.json'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('menu.json'))
 
     assert_equal 5, json[3]['id']
     assert !json[3]['category']['filters'].empty?
   end
 
   def test_poi
-    get :poi, params: { project: 'test', theme: 'theme', id: 1 }
+    get :poi, params: { project: 'test', theme: 'theme', id: 1, format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('poi/{id}.geojson'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('poi/{id}.geojson'))
 
     assert json.dig('properties', 'editorial', 'popup_fields')
   end
 
   def test_poi_deps
-    get :poi, params: { project: 'test', theme: 'theme', id: 1, deps: 'true' }
+    get :poi, params: { project: 'test', theme: 'theme', id: 1, deps: 'true', format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('poi/{id}/deps.geojson'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('poi/{id}/deps.geojson'))
 
     assert_equal 2, json['features'].size
   end
 
   def test_pois_category
-    get :pois_category, params: { project: 'test', theme: 'theme', category_id: 5 }
+    get :pois_category, params: { project: 'test', theme: 'theme', category_id: 5, format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
     assert !json['features'].empty?
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('pois/category/{id}.{format}'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('pois/category/{id}.{format}'))
 
     assert_equal 2, json['features'].size
   end
 
   def test_pois
-    get :pois, params: { project: 'test', theme: 'theme' }
+    get :pois, params: { project: 'test', theme: 'theme', format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('pois.{format}'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('pois.{format}'))
 
     assert_equal 2, json['features'].size
     assert 'Point', json['features'][0]['geometry']['type']
@@ -90,38 +89,38 @@ class Api01ControllerTest < ActionController::TestCase
   end
 
   def test_ids
-    get :pois, params: { project: 'test', theme: 'theme', ids: '1,2' }
+    get :pois, params: { project: 'test', theme: 'theme', ids: '1,2', format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('pois.{format}'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('pois.{format}'))
 
     assert_equal 2, json['features'].size
   end
 
   def test_geometry_as
-    get :poi, params: { project: 'test', theme: 'theme', id: 2, geometry_as: 'point' }
+    get :poi, params: { project: 'test', theme: 'theme', id: 2, geometry_as: 'point', format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('poi/{id}.geojson'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('poi/{id}.geojson'))
 
     assert 'Point', json['geometry']['type']
   end
 
   def test_short_description
-    get :pois, params: { project: 'test', theme: 'theme', short_description: 'true' }
+    get :pois, params: { project: 'test', theme: 'theme', short_description: 'true', format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('pois.{format}'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('pois.{format}'))
 
     assert_equal 20, json['features'][0]['properties']['description'].size
     # TODO: assert no html tags
   end
 
   def test_dates
-    get :pois, params: { project: 'test', theme: 'theme', start_date: '2022-01-01', end_date: '2022-12-31' }
+    get :pois, params: { project: 'test', theme: 'theme', start_date: '2022-01-01', end_date: '2022-12-31', format: :geojson }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('pois.{format}'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('pois.{format}'))
 
     assert_equal 2, json['features'].size
   end
@@ -130,6 +129,6 @@ class Api01ControllerTest < ActionController::TestCase
     get :attribute_translations, params: { project: 'test', theme: 'theme', lang: 'fr' }
     assert_response :success
     json = JSON.parse(@response.body)
-    JSON::Validator.validate!(@schema, json, fragment: schema_for('attribute_translations/{lang}.json'))
+    JSON::Validator.validate!(@@schema, json, fragment: schema_for('attribute_translations/{lang}.json'))
   end
 end
--- tests/elasa-fixtures.sql
+++ tests/elasa-fixtures.sql
@@ -17,10 +17,18 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Data for Name: languages; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.languages (code, name, direction) VALUES ('fr-FR', 'French', 'ltr');
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.projects (id, icon_font_css_url, polygon, name, slug, articles, default_country, default_country_state_opening_hours) VALUES (1, 'https://example.com/font-teritorio/dist/teritorio.css?ver=2.7.0', '0103000020E610000001000000160000000000000008A3F8BFBFB7A8454DC2454000000000802BF8BF836BC9D99EC2454000000000D897F7BF8FDA4D02A5C045400000000098D8F6BFEC2AA6A7FCBE454000000000408BF6BF2E1A103D32BE45400000000040AAF5BF12F142F0F0BD454000000000D093F4BF05BE41E352BE45400000000040E8F3BFDA537B70CFC0454000000000F0CEF3BF1436B6D8E4C145400000000060D7F3BF14D4C9644AC64540000000008020F4BFB7EA98DD43C84540000000003042F5BFCE29BAE4E6C8454000000000E082F5BFF47278677ECA45400000000090C3F5BF1B5BC6DE72CB4540000000002402F6BFC59375F564CE4540000000000E54F6BFE781887253CF454000000000B89AF6BF2E0926AB38CE4540000000007EB6F6BF914CC5EA90CB454000000000D0DCF6BFE608827E73C945400000000080D1F7BFA4CC088905CC45400000000040F3F7BF33A371CE5DCA45400000000008A3F8BFBFB7A8454DC24540', '{"fr":"test"}', 'test', '[{"title":{"fr":"A propos"},"url":{"fr":"https://example.com"}}]', 'fr', 'Nouvelle-Aquitaine');
+INSERT INTO public.projects (id, icon_font_css_url, polygon, slug, articles, default_country, default_country_state_opening_hours) VALUES (1, 'https://example.com/font-teritorio/dist/teritorio.css?ver=2.7.0', '0103000020E610000001000000160000000000000008A3F8BFBFB7A8454DC2454000000000802BF8BF836BC9D99EC2454000000000D897F7BF8FDA4D02A5C045400000000098D8F6BFEC2AA6A7FCBE454000000000408BF6BF2E1A103D32BE45400000000040AAF5BF12F142F0F0BD454000000000D093F4BF05BE41E352BE45400000000040E8F3BFDA537B70CFC0454000000000F0CEF3BF1436B6D8E4C145400000000060D7F3BF14D4C9644AC64540000000008020F4BFB7EA98DD43C84540000000003042F5BFCE29BAE4E6C8454000000000E082F5BFF47278677ECA45400000000090C3F5BF1B5BC6DE72CB4540000000002402F6BFC59375F564CE4540000000000E54F6BFE781887253CF454000000000B89AF6BF2E0926AB38CE4540000000007EB6F6BF914CC5EA90CB454000000000D0DCF6BFE608827E73C945400000000080D1F7BFA4CC088905CC45400000000040F3F7BF33A371CE5DCA45400000000008A3F8BFBFB7A8454DC24540', 'test', '[{"title":{"fr":"A propos"},"url":{"fr":"https://example.com"}}]', 'fr', 'Nouvelle-Aquitaine');
+INSERT INTO public.projects_translations (id, projects_id, languages_code, name) VALUES (1, 1, 'fr-FR', 'test');
 
 
 --
@@ -44,7 +52,8 @@ INSERT INTO public.fields_fields (id, fields_id, related_fields_id, index) VALUE
 -- Data for Name: filters; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.filters (id, type, name, property_end, property_begin, min, max, project_id, multiselection_property, checkboxes_list_property, boolean_property, number_range_property) VALUES (1, 'checkboxes_list', '{"fr":"Nombre d''étoiles"}', NULL, NULL, NULL, NULL, 1, 'stars', 'stars', 'stars', 'stars');
+INSERT INTO public.filters (id, type, property_end, property_begin, min, max, project_id, multiselection_property, checkboxes_list_property, boolean_property, number_range_property) VALUES (1, 'checkboxes_list', NULL, NULL, NULL, NULL, 1, 'stars', 'stars', 'stars', 'stars');
+INSERT INTO public.filters_translations (id, filters_id, languages_code, name) VALUES (1, 1, 'fr-FR', 'Nombre d''étoiles');
 
 --
 -- Data for Name: junction_directus_roles_undefined; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -56,18 +65,24 @@ INSERT INTO public.filters (id, type, name, property_end, property_begin, min, m
 -- Data for Name: themes; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.themes (id, project_id, slug, name, description, logo_url, favicon_url, root_menu_item_id, site_url, main_url, keywords, favorites_mode, explorer_mode) VALUES (1, 1, 'theme', '{"fr": "Carte et points d''intérêts du Seignanx. Sud-Ouest des Landes"}', '{"fr": "Carte et annuaire : hébergement, restauration, loisirs, sports, balades et commerces"}', 'https://carte.seignanx.com/content/wp-content/uploads/2022/02/seignanx-com.png', 'https://carte.seignanx.com/content/wp-content/uploads/2022/03/Favicon.jpg', NULL, '{"fr":"https://carte.seignanx.com/"}', '{"fr":"https://www.seignanx.com/"}', '{"fr":""}', false, false);
+INSERT INTO public.themes (id, project_id, slug, logo_url, favicon_url, root_menu_item_id, favorites_mode, explorer_mode) VALUES (1, 1, 'theme', 'https://carte.seignanx.com/content/wp-content/uploads/2022/02/seignanx-com.png', 'https://carte.seignanx.com/content/wp-content/uploads/2022/03/Favicon.jpg', NULL, false, false);
+INSERT INTO public.themes_translations (id, themes_id, languages_code, name, description, site_url, main_url, keywords) VALUES (1, 1, 'fr-FR', 'Carte et points d''intérêts du Seignanx. Sud-Ouest des Landes', 'Carte et annuaire : hébergement, restauration, loisirs, sports, balades et commerces', 'https://carte.seignanx.com/', 'https://www.seignanx.com/', '');
 
 
 --
 -- Data for Name: menu_items; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, slugs, name, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (1, 0, false, false, NULL, 1, '{"original_id": "1"}', '{"en":"Root","fr":"Racine"}', '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'menu_group', NULL, NULL, NULL);
-INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, slugs, name, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (2, 1, false, false, 1, 1, '{"original_id": "2"}', '{"fr":"Bloc Recherche"}', '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'menu_group', NULL, NULL, NULL);
-INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, slugs, name, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (3, 2, true, false, 2, 1, '{"original_id": "3"}', '{"fr":"Recherche"}', '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'search', NULL, NULL, NULL);
-INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, slugs, name, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (4, 3, false, false, 1, 1, '{"original_id": "4"}', '{"fr":"bloc poi"}', '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'menu_group', NULL, NULL, NULL);
-INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, slugs, name, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (5, 4, false, false, 4, 1, '{"original_id": "5"}', '{"fr":"Hébergement"}', 'teritorio teritorio-hosting', 'compact', NULL, true, NULL, '#99163a', '#99163a', NULL, 'hosting,hosting_resort,camp_site', 'category', 1, 1, 1);
+INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (1, 0, false, false, NULL, 1, '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'menu_group', NULL, NULL, NULL);
+INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (2, 1, false, false, 1, 1, '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'menu_group', NULL, NULL, NULL);
+INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (3, 2, true, false, 2, 1,  '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'search', NULL, NULL, NULL);
+INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (4, 3, false, false, 1, 1, '', 'compact', NULL, true, NULL, '', '', NULL, NULL, 'menu_group', NULL, NULL, NULL);
+INSERT INTO public.menu_items (id, index_order, hidden, selected_by_default, parent_id, theme_id, icon, display_mode, search_indexed, style_merge, zoom, color_fill, color_line, href, style_class_string, type, popup_fields_id, details_fields_id, list_fields_id) VALUES (5, 4, false, false, 4, 1,  'teritorio teritorio-hosting', 'compact', NULL, true, NULL, '#99163a', '#99163a', NULL, 'hosting,hosting_resort,camp_site', 'category', 1, 1, 1);
+INSERT INTO public.menu_items_translations (id, menu_items_id, languages_code, name, slug, name_singular) VALUES (1, 1, 'fr-FR', 'Racine', '1', NULL);
+INSERT INTO public.menu_items_translations (id, menu_items_id, languages_code, name, slug, name_singular) VALUES (2, 2, 'fr-FR', 'Bloc Recherche', '2', NULL);
+INSERT INTO public.menu_items_translations (id, menu_items_id, languages_code, name, slug, name_singular) VALUES (3, 3, 'fr-FR', 'Recherche', '3', NULL);
+INSERT INTO public.menu_items_translations (id, menu_items_id, languages_code, name, slug, name_singular) VALUES (4, 4, 'fr-FR', 'bloc poi', '4', NULL);
+INSERT INTO public.menu_items_translations (id, menu_items_id, languages_code, name, slug, name_singular) VALUES (5, 5, 'fr-FR', 'Hébergement', '5', NULL);
 
 
 --
@@ -87,7 +102,8 @@ INSERT INTO public.menu_items_filters (id, menu_items_id, filters_id) VALUES (1,
 -- Data for Name: sources; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.sources (id, project_id, slug, name, attribution) VALUES (1, 1, 'agences_immobilieres', '{"fr": "agences_immobilieres"}', 'Sirtaqui');
+INSERT INTO public.sources (id, project_id, slug, attribution) VALUES (1, 1, 'agences_immobilieres', 'Sirtaqui');
+INSERT INTO public.sources_translations (id, sources_id, languages_code, name) VALUES (1, 1, 'fr-FR', 'agences_immobilieres');
 
 
 --
@@ -137,6 +153,7 @@ SELECT pg_catalog.setval('public.fields_id_seq', 3, true);
 --
 
 SELECT pg_catalog.setval('public.filters_id_seq', 1, true);
+SELECT pg_catalog.setval('public.filters_translations_id_seq', 1, true);
 
 
 --
@@ -165,6 +182,7 @@ SELECT pg_catalog.setval('public.menu_items_filters_id_seq', 1, true);
 --
 
 SELECT pg_catalog.setval('public.menu_items_id_seq', 6, true);
+SELECT pg_catalog.setval('public.menu_items_translations_id_seq', 6, true);
 
 
 --
@@ -179,6 +197,7 @@ SELECT pg_catalog.setval('public.menu_items_sources_id_seq', 2, true);
 --
 
 SELECT pg_catalog.setval('public.projects_id_seq', 2, true);
+SELECT pg_catalog.setval('public.projects_translations_id_seq', 2, true);
 
 
 --
@@ -186,6 +205,7 @@ SELECT pg_catalog.setval('public.projects_id_seq', 2, true);
 --
 
 SELECT pg_catalog.setval('public.sources_id_seq', 2, true);
+SELECT pg_catalog.setval('public.sources_translations_id_seq', 2, true);
 
 
 --
@@ -193,6 +213,7 @@ SELECT pg_catalog.setval('public.sources_id_seq', 2, true);
 --
 
 SELECT pg_catalog.setval('public.themes_id_seq', 2, true);
+SELECT pg_catalog.setval('public.themes_translations_id_seq', 2, true);
 
 
 --
