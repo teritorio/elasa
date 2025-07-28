9d50414f51cb1b21823e0d908c67c91cd4a7ecb3
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-07-28 19:50:30 +0200

    Fix color migration script

--- db/migrate/2025071710000000_colors.sql
+++ db/migrate/2025071710000000_colors.sql
@@ -1,7 +1,7 @@
 UPDATE
     directus_fields
 SET
-    options = '{"presets":[{"name":"$t:amenity","color":"#2A62AC"},{"name":"$t:catering","color":"#DB7900"},{"name":"$t:local_shop","color":"#009CB8"},{"name":"$t:craft","color":"#C58511"},{"name":"$t:culture","color":"#76009E"},{"name":"$t:culture_shopping","color":"#A04C97"},{"name":"$t:education","color":"#4076F6"},{"name":"$t:hosting","color":"#99163A"},{"name":"$t:leisure","color":"#00A757"},{"name":"$t:mobility","color":"#008ECF"},{"name":"$t:nature","color":"#8CC56F"},{"name":"$t:products","color":"#F25C05"},{"name":"$t:public_landmark","color":"#1D1D1B"},{"name":"$t:remarkable","color":"#E50980"},{"name":"$t:safety","color":"#E42224"},{"name":"$t:services","color":"#2A62AC"},{"name":"$t:services_shopping","color":"#4E7AB5"},{"name":"$t:shopping","color":"#808080"},{"name":"$t:social_services","color":"#006660"}]}'::json
+    options = '{"presets":[{"name":"$t:amenity","color":"#2A62AC"},{"name":"$t:catering","color":"#CC4C16"},{"name":"$t:local_shop","color":"#278096"},{"name":"$t:craft","color":"#996E0A"},{"name":"$t:culture","color":"#76009E"},{"name":"$t:culture_shopping","color":"#A04C97"},{"name":"$t:education","color":"#3366FF"},{"name":"$t:hosting","color":"#99163A"},{"name":"$t:leisure","color":"#008A1E"},{"name":"$t:mobility","color":"#007DB8"},{"name":"$t:nature","color":"#006B31"},{"name":"$t:products","color":"#BD533C"},{"name":"$t:public_landmark","color":"#000000"},{"name":"$t:remarkable","color":"#C02469"},{"name":"$t:safety","color":"#E42224"},{"name":"$t:services","color":"#2A62AC"},{"name":"$t:services_shopping","color":"#5173B7"},{"name":"$t:shopping","color":"#767676"},{"name":"$t:social_services","color":"#006660"}]}'::json
 WHERE
     id IN (78, 79)
 ;
