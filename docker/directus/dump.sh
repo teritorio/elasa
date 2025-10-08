6227243e45b272727a03f958ccb351fdf6344e03
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-10-08 23:38:00 +0200

    Exclude directus_folders and directus_files from dump

--- docker/directus/dump.sh
+++ docker/directus/dump.sh
@@ -1,6 +1,6 @@
 source ../../.env
 docker compose exec postgres pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" -n public --schema-only -T local-* -T pois_local | grep -v "CREATE SCHEMA public" > schema.sql
-docker compose exec postgres pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" -n public --data-only -t directus_* -T directus_sessions -T directus_activity -T directus_revisions -T directus_presets > data-tmp.sql
+docker compose exec postgres pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" -n public --data-only -t directus_* -T directus_sessions -T directus_activity -T directus_revisions -T directus_presets -T directus_folders -T directus_files > data-tmp.sql
 perl pg_dump_sort.perl data-tmp.sql | \
     grep -v '^local-' | \
     grep -v "^[0-9]\+\slocal-" \
