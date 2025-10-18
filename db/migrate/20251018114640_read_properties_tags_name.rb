d704eca3c2a8010b173a13308663b4f8e6f14d57
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-10-18 13:46:18 +0200

    Allow read to local admin of poi.properties_tags_name

---
+++ db/migrate/20251018114640_read_properties_tags_name.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class ReadPropertiesTagsName < ActiveRecord::Migration[8.0]
+  def change
+    execute <<~SQL.squish
+      UPDATE directus_permissions SET fields = 'id,source_id,geom,properties,slugs,override,image,website_details,properties_tags_name' WHERE id = 83;
+    SQL
+  end
+end
