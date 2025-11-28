717bc6660b8b8b756e1043b326786c48d144cdba
Abdechakour Mrabet <abdchakour.m@gmail.com>
2025-11-28 09:27:18 +0100

    Fix migration classname

--- db/migrate/20251127145552_read_properties_id.rb
+++ db/migrate/20251127145552_read_properties_id.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ReadPropertiesTagsName < ActiveRecord::Migration[8.0]
+class ReadPropertiesId < ActiveRecord::Migration[8.0]
   def change
     execute <<~SQL.squish
       UPDATE directus_permissions SET fields = 'id,source_id,geom,properties,slugs,override,image,website_details,properties_tags_name,properties_id' WHERE id = 83;
