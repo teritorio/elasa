40a17935381dd38c635f640bde57dc38a3b93710
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-12-16 13:31:35 +0100

    Add field icon

--- db/migrate/20251210214838_field_type.rb
+++ db/migrate/20251210214838_field_type.rb
@@ -14,6 +14,14 @@ class FieldType < ActiveRecord::Migration[8.0]
       (613, 'fields', 'media_type', NULL, 'select-dropdown', '{"choices":[{"text":"text/plain","value":"Plain text"},{"text":"text/html","value":"HTML"},{"text":"text/x-uri","value":"URL"},{"text":"text/vnd.phone-number","value":"Phone number"},{"text":"text/vnd.osm.opening_hours","value":"OSM opening_hours value"},{"text":"text/vnd.osm.html-color","value":"HTML color"},{"text":"text/vnd.osm.stars","value":"OSM stars value"}]}', NULL, NULL, false, false, 4, 'full', NULL, NULL, NULL, false, 'field_block', NULL, NULL),
       (614, 'fields', 'role', NULL, 'select-dropdown', '{"choices":[{"text":"string","value":"string"},{"text":"html","value":"html"},{"text":"string@short","value":"string@short"},{"text":"integer","value":"integer"},{"text":"boolean","value":"boolean"},{"text":"weblink","value":"weblink"},{"text":"weblink@scocial-network","value":"weblink@scocial-network"},{"text":"weblink@download","value":"weblink@download"},{"text":"email","value":"email"},{"text":"phone","value":"phone"},{"text":"date","value":"date"},{"text":"datetime","value":"datetime"},{"text":"duration","value":"duration"},{"text":"start_end_date","value":"start_end_date"},{"text":"osm:opening_hours","value":"osm:opening_hours"},{"text":"osm:collection_times","value":"osm:collection_times"},{"text":"image","value":"image"},{"text":"mapillary","value":"mapillary"},{"text":"panoramax","value":"panoramax"},{"text":"tag","value":"tag"},{"text":"color","value":"color"},{"text":"rating-scale","value":"rating-scale"},{"text":"osm:stars","value":"osm:stars"},{"text":"coordinates","value":"coordinates"},{"text":"addr","value":"addr"},{"text":"route","value":"route"}]}', NULL, NULL, false, false, 5, 'full', NULL, NULL, NULL, false, 'field_block', NULL, NULL)
       ;
+
+      UPDATE directus_fields SET sort = 6 WHERE id = 140;
+      UPDATE directus_fields SET "group" = NULL WHERE id = 140;
+      UPDATE directus_fields SET sort = 8 WHERE id = 141;
+      UPDATE directus_fields SET sort = 3 WHERE id = 144;
+      UPDATE directus_fields SET sort = 9 WHERE id = 148;
+      UPDATE directus_fields SET sort = 7 WHERE id = 550;
+      UPDATE directus_fields SET sort = 10 WHERE id = 607;
     SQL
   end
 end
