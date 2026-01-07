b6dff3fb3167808abab6e5e6c1804075b34af817
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-01-07 21:29:44 +0100

    Update sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,7 @@ if ENV['SENTRY_DSN_API'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN_API']
     # enable performance monitoring
-    config.enable_tracing = true
+    config.traces_sample_rate = 1
     # get breadcrumbs from logs
     config.breadcrumbs_logger = [:http_logger]
   end
